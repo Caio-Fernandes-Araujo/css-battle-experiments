821d7de4778a02ffb7b3429bf9527afd12d48283
Caio Fernandes <caiofz@hotmail.com>
2026-05-06 15:34:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,347 @@
+// Início da Lógica da troca de Seções
+
+        //v2 - mais otimizado, função reutilizável:
+        const buttons = document.querySelectorAll('.btn-container');
+        const botaoAnime = document.querySelector('#btn-anime');
+        const botaoHero = document.querySelector('#btn-hero');
+        const botaoHypno = document.querySelector('#btn-hypno');
+
+        const sections = document.querySelectorAll('.section-container');
+        const secaoAnime = document.querySelector('#sec-anime');
+        const secaoHero = document.querySelector('#sec-hero');
+        const secaoHypno = document.querySelector('#sec-hypno');
+
+        function AddStyleButtom(botaoParaAtivar) {
+            buttons.forEach(el => {
+                el.classList.remove('btnActive')
+            });
+
+            botaoParaAtivar.classList.add('btnActive');
+        }
+
+        function showSection(secaoParaAtivar) {
+            sections.forEach(el => {
+                el.classList.remove('sectionActive')
+            });
+
+            secaoParaAtivar.classList.add('sectionActive');
+        }
+
+        botaoAnime.addEventListener('click', () => {
+            AddStyleButtom(botaoAnime);
+            showSection(secaoAnime);
+        })
+
+        botaoHero.addEventListener('click', () => {
+            AddStyleButtom(botaoHero);
+            showSection(secaoHero);
+        })
+
+        botaoHypno.addEventListener('click', () => {
+            AddStyleButtom(botaoHypno);
+            showSection(secaoHypno);
+        });
+
+       /*v1 - mais verboso, feito na mão:
+        const botaoAnime = document.querySelector('#btn-anime');
+        const botaoHero = document.querySelector('#btn-hero');
+        const botaoHypno = document.querySelector('#btn-hypno');
+
+        const secaoAnime = document.querySelector('#sec-anime');
+        const secaoHero = document.querySelector('#sec-hero');
+        const secaoHypno = document.querySelector('#sec-hypno');
+
+            function btnAnimeActive() {
+                botaoAnime.classList.add('btnActive');
+                botaoHero.classList.remove('btnActive');
+                botaoHypno.classList.remove('btnActive');
+            }
+
+            function btnHeroActive() {
+                botaoAnime.classList.remove('btnActive');
+                botaoHero.classList.add('btnActive');
+                botaoHypno.classList.remove('btnActive');
+            }
+
+            function btnHypnoActive() {
+                botaoAnime.classList.remove('btnActive');
+                botaoHero.classList.remove('btnActive');
+                botaoHypno.classList.add('btnActive');
+            }
+
+            function SecAnimeActive() {
+                secaoAnime.classList.add('sectionActive');
+                secaoHero.classList.remove('sectionActive');
+                secaoHypno.classList.remove('sectionActive');
+            }
+
+            function SecHeroActive() {
+                secaoAnime.classList.remove('sectionActive');
+                secaoHero.classList.add('sectionActive');
+                secaoHypno.classList.remove('sectionActive');
+            }
+
+            function SecHypnoActive() {
+                secaoAnime.classList.remove('sectionActive');
+                secaoHero.classList.remove('sectionActive');
+                secaoHypno.classList.add('sectionActive');
+            }
+
+        botaoAnime.addEventListener('click', () => {
+            SecAnimeActive();
+            btnAnimeActive();
+        })
+
+        botaoHero.addEventListener('click', () => {
+            SecHeroActive();
+            btnHeroActive();
+        })
+
+        botaoHypno.addEventListener('click', () => {
+            SecHypnoActive();
+            btnHypnoActive();
+        })*/
+
+        // Fim da Lógica da troca de Seções
+
+        
+        //Função de toggle simples reutilizável  
+        function alternarClasse(elemento, classe) {
+            elemento.classList.toggle(classe);
+        };
+
+        //Função de toggle com forEach reutilizável
+        function alternarClasseForEach(elemento, classe) {
+            elemento.forEach(el => {
+                el.classList.toggle(classe)
+            })
+        };
+
+        // Início do Ryuk
+
+        const ryukContainer = document.querySelector('.ryuk-background');
+        const ryukCabelo = document.querySelector('.ryuk-hair');
+        const ryukContornoOlho = document.querySelectorAll('.ryuk-eye-contour');
+        const ryukOlhosVerdeEscuro = document.querySelectorAll('.ryuk-eye-dark-green-circle');
+        const ryukOlhosVerdeClaro = document.querySelectorAll('.ryuk-eye-light-green-circle');
+        const ryukOlhosMarrons = document.querySelectorAll('.ryuk-eye-brown-circle');
+
+        function ryukNegativeColors() {
+
+            ryukContainer.classList.toggle('ryukBackgroundNegative');
+
+            ryukCabelo.classList.toggle('ryukHairNegative');
+
+            ryukContornoOlho.forEach(el => {
+                el.classList.toggle('ryukEyeContourNegative')
+            });
+
+            ryukOlhosVerdeEscuro.forEach(el => {
+                el.classList.toggle('ryukEyeDarkGreenNegative')
+            });
+
+            ryukOlhosVerdeClaro.forEach(el => {
+                el.classList.toggle('ryukEyeLightGreenNegative')
+            });
+
+            ryukOlhosMarrons.forEach(el => {
+                el.classList.toggle('ryukEyeBrownNegative')
+            });
+
+        }
+
+        ryukContainer.addEventListener('click', () => {
+
+            ryukNegativeColors();
+
+        })
+
+        // Fim do Ryuk
+
+        // Início do Amegakure
+
+        const amegakureContainer = document.querySelector('.amegakure-background');
+        const amegakureRisco = document.querySelector('.amegakure-risk');
+
+            /*function amegakureRiskCross() {
+                amegakureRisco.classList.toggle('amegakureRiskActive');
+            }*/
+
+        amegakureContainer.addEventListener('click', () => {
+
+            //função de toggle simples reutilizável
+            alternarClasse(amegakureRisco, 'amegakureRiskActive');
+        });
+
+        // Fim do Amegakure
+
+        // Início do Kurama
+
+        const kuramaContainer = document.querySelector('.kurama-background');
+        const kuramaOlhoVermelho = document.querySelector('.kurama-eye-red');
+
+        function kuramaChangeColors() {
+                kuramaContainer.classList.toggle('kuramaContainerActive');
+
+                kuramaOlhoVermelho.classList.toggle('kuramaEyeActive');
+            }           
+
+        kuramaContainer.addEventListener('click', () => {
+            kuramaChangeColors();         
+        });
+
+        // Fim do Kurama
+
+        // Início do Punisher
+
+        const punisherContainer = document.querySelector('.punisher-container');
+        const punisherShapesBlack = document.querySelectorAll('.js-punisher-black')
+        const punisherShapesShadow = document.querySelectorAll('.js-punisher-shadow');
+        const punisherTeethShadow = document.querySelectorAll('.punisher-teeth');
+
+        function punisherChangeColors() {
+                punisherContainer.classList.toggle('punisherContainerActive');
+
+                punisherShapesBlack.forEach( el => {
+                    el.classList.toggle('punisherShapeBlackActive')
+                });
+
+                punisherShapesShadow.forEach( el => {
+                    el.classList.toggle('punisherShapeShadowActive')
+                });
+                
+                punisherTeethShadow.forEach( el => {
+                    el.classList.toggle('punisherTeethShadowActive')
+                });
+            };           
+
+        punisherContainer.addEventListener('click', () => {
+            punisherChangeColors();
+        });
+
+        // Fim do Punisher
+
+        // Início do Wolverine
+
+        const wolverineContainer = document.querySelector('.wolverine-background');
+        const wolverineFaceShapes = document.querySelectorAll('.js-wolverine');
+
+            function wolverineChangeColors() {
+
+                wolverineContainer.classList.toggle('wolverineContainerActive');
+
+                wolverineFaceShapes.forEach(el => {
+                    el.classList.toggle('wolverineFaceActive')
+                });
+            }
+
+        wolverineContainer.addEventListener('click', () => {
+
+            wolverineChangeColors();
+
+        });
+
+        // Fim do Wolverine
+
+        // Início do Green Lantern
+
+        const greenLanternContainer = document.querySelector('.green-lantern-background');
+        const greenLanternShapes = document.querySelectorAll('.js-lantern');
+        const insetBoxShadow = document.querySelector('.green-lantern-core');
+
+        function lanternChangeColors() {
+                greenLanternContainer.classList.toggle('lanternActive');
+
+                greenLanternShapes.forEach( el => {
+                    el.classList.toggle('lanternActive');
+                });
+
+                insetBoxShadow.classList.toggle('lanternCoreActive');
+            }    
+
+        greenLanternContainer.addEventListener('click', () => {
+
+            lanternChangeColors();
+
+        });
+
+        //Acessibilidade
+        
+        greenLanternContainer.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+                e.preventDefault();
+                lanternChangeColors();
+            }});
+
+        // Fim do Green Lantern
+
+
+        // Início do look away
+
+        const lookAwayContainer = document.querySelector('.look-away-background');
+        const lookAwayCorpo = document.querySelector('.look-away-body');
+        const lookAwayShapes = document.querySelectorAll('.js-lookAway');
+
+
+        function lookAwayActive() {
+                lookAwayContainer.classList.toggle('lookAwayContainerActive');
+
+                lookAwayCorpo.classList.toggle('lookAwayBodyActive');
+
+                lookAwayShapes.forEach( el => {
+                    el.classList.toggle('lookAwayBlueShapesActive');
+                });
+            }    
+
+
+        lookAwayContainer.addEventListener('click', () =>{
+
+            lookAwayActive();
+
+        });
+
+        // Início do look away
+
+        // Início do atom
+
+        const atomContainer = document.querySelector('.atom-background');
+        const atomShapes = document.querySelectorAll('.js-atom');
+
+        function atomChangeColors() {
+                atomContainer.classList.toggle('atomContainerActive');
+
+                atomShapes.forEach( el => {
+                    el.classList.toggle('atomRingsActive');
+                });
+            }   
+
+        atomContainer.addEventListener('click', () => {
+
+            atomChangeColors();
+
+
+        });
+
+        // Fim do atom
+
+        // início do fountain
+
+        const fountainContainer = document.querySelector('.fountain-background')
+        const fountainShapes = document.querySelectorAll('.js-fountain');
+
+        function fountainActive() {
+            fountainContainer.classList.toggle('fountainBackgroundActive');
+
+            fountainShapes.forEach(el => {
+            el.classList.toggle('fountainActive')
+            });     
+
+        }
+
+        fountainContainer.addEventListener('click', () =>{
+
+            fountainActive();
+
+        });
+
+        // Fim do fountain
